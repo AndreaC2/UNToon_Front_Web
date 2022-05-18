1f662b2c82b9883749b425f87b0d0e08c0013702
AndreaC2 <ankcalderonga@unal.edu.co>
2022-05-18 02:08:35 -0500

    base inicial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+*{
+  --color_red: #B32D43;
+  --color_beige: #F2E8CF;
+  --color_green_1: #94B43B;
+  --color_green_2: #A7D757;
+}
+body{
+  margin: 0%;
+}
+</style>
