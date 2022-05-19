43d2481b99bf0f1400ef17385015f4ef634e0b7f
AndreaC2 <ankcalderonga@unal.edu.co>
2022-05-19 00:12:16 -0500

    Routas

--- src/View/Home.vue
+++ src/View/Home.vue
@@ -1,31 +1,25 @@
 <template>
   <div>
     <NavBar></NavBar>
+    <SearchBar></SearchBar>
+    <ComicsHome></ComicsHome>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
-
+import SearchBar from "../components/SearchBar.vue";
+import ComicsHome from "../components/ComicsHome.vue"
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default {
-  name: "Profile",
+  name: "Home",
   components: {
-    NavBar
+    NavBar,
+    SearchBar,
+    ComicsHome
   },
 };
 </script>
 
-<style>
-* {
-  --color_red: #b32d43;
-  --color_beige: #f2e8cf;
-  --color_green_1: #94b43b;
-  --color_green_2: #a7d757;
-}
-body {
-  margin: 0%;
-}
-</style>
